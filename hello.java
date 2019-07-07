9a1e9ec8a80571564114055969225f914a6a58d8
1163710211 <18845794238@163.com>
2019-07-07 22:38:18 +0800

    v2.0

--- hello.java
+++ hello.java
@@ -5,6 +5,10 @@ public class oss {
 	// 	return super.toString();
 	// }
 	public static void main(String[] args) {
+		
+		hello();
+	}
+	public static void hello(){
 		System.out.println("hello world");
 	}
 }
