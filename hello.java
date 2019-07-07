ce247f77d9fd4302c6451fa37734beea7669b54a
1163710211 <18845794238@163.com>
2019-07-07 18:56:38 +0800

    make the code run successfully

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 package com.njupt.zhb.test;
 
 public class oss {
-	public String toString() {
-		return super.toString();
+	// public String toString() {
+	// 	return super.toString();
+	// }
+	public static void main(String[] args) {
+		System.out.println("hello world");
 	}
-}
